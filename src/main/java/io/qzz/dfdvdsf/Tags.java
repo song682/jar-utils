eefe32ff9674535de031b026316bdd47a8524771
song682 <songpengjie2007@163.com>
2026-08-14 11:15:56 +0800

    upgrade

--- src/main/java/io/qzz/dfdvdsf/Tags.java
+++ src/main/java/io/qzz/dfdvdsf/Tags.java
@@ -3,5 +3,5 @@ package io.qzz.dfdvdsf;
 public class Tags {
     public static final String MODID = "jarutils";
     public static final String NAME = "JarUtils";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.2";
 }
